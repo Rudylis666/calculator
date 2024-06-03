7c01fb0f6ca3ac4afffc8b3cd3bbeb003304cc35
Aleksandra Lasiewicka <alex.lasiewicka@gmail.com>
2024-06-03 10:21:54 +0200

    simple actions works!

--- calc.py
+++ calc.py
@@ -1,4 +1,16 @@
 
+def dodawanie(a,b):
+    return a+b
+
+def odejmowanie(a,b):
+    return a-b
+
+def mnozenie(a,b):
+    return a*b
+
+def dzielenie(a,b):
+    if b==0 : return ("Nie można dzielić przez 0!")
+    else: return a/b
 
 print("Witaj w prostym kalkulatorze!")
 print("Oto jakie działania obsługujemy:")
@@ -9,3 +21,20 @@ print("3. Mnożenie")
 print("4. Dzielenie")
 print("5. Wyjście")
 choice = int(input("Wpisz cyfrę swojego wyboru: "))
+match choice:
+    case 1:
+        a=int(input("Podaj pierwszą liczbę: "))
+        b = int(input("Podaj drugą liczbę: "))
+        print("Twój wynik to: ",dodawanie(a,b))
+    case 2:
+        a = int(input("Podaj pierwszą liczbę: "))
+        b = int(input("Podaj drugą liczbę: "))
+        print("Twój wynik to: ", odejmowanie(a, b))
+    case 3:
+        a = int(input("Podaj pierwszą liczbę: "))
+        b = int(input("Podaj drugą liczbę: "))
+        print("Twój wynik to: ", mnozenie(a, b))
+    case 4:
+        a = int(input("Podaj pierwszą liczbę: "))
+        b = int(input("Podaj drugą liczbę: "))
+        print("Twój wynik to: ", dzielenie(a, b))
