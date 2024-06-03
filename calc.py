03c19f1d4d93d4fbcdefd9933fedc665132954a3
Aleksandra Lasiewicka <alex.lasiewicka@gmail.com>
2024-06-03 10:12:50 +0200

    first sketch

---
+++ calc.py
@@ -0,0 +1,11 @@
+
+
+print("Witaj w prostym kalkulatorze!")
+print("Oto jakie działania obsługujemy:")
+print("================================")
+print("1. Dodawanie")
+print("2. Odejmowanie")
+print("3. Mnożenie")
+print("4. Dzielenie")
+print("5. Wyjście")
+choice = int(input("Wpisz cyfrę swojego wyboru: "))
